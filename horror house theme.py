ff4c3bafb6eb0afeff37f327af200f39118226b1
Somesh-Hend <someshhend746@gmail.com>
2026-09-16 23:53:46 +0530

    Add files via upload

---
+++ horror house theme.py
@@ -0,0 +1,19 @@
+print("Wellcome To Horror House Website".center(65))
+print("----------------------------------------------------------------")
+print("Book Your Slot for today".center(40))
+input("click enter to book")
+print("click code that you having shift")
+print("    A            B                C               D")
+print("first shift    secand shift    third shift    fouth shift")
+print(" 8 to 10     10:30 to 12:30    1 to 3         3:30 to 5:30")
+option=input("Enter code for particular shift:")
+if option=="A":
+    print("shift one is you get")
+elif option=="B":
+    print("shift two is you get")
+elif option=="C":
+    print,("shift three is you get")
+elif option=="D":
+    print("shift four is you get")
+else:
+    print("chose correct option")
